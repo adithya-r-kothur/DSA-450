0129c130abb2cef0a2141960585904cffed14897
adithya <adithya.r.kothur@gmail.com>
2022-06-02 11:05:18 +0530

    initial commit

---
+++ ARRAYS/miniandmaxi.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    cout<<a[0]<<" "<<a[n-1]<<"\n";
+
+    return 0;
+}
+
+
+/* also study the methods in geeks for geeks 
+they have used struct method */
